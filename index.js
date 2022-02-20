a8530ba81dfd0097c5955142aeba93eaa24352ae
janmrakava <janmrakava@gmail.com>
2022-02-20 17:50:57 +0100

    Podmínky pro vzití nové karty

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el") //už umíme dávno
@@ -15,7 +13,7 @@ function getRandomCard(){
   let randomNumber = Math.floor(Math.random()*13)+1;
   if (randomNumber === 1){
     return 11
-  } else if (randomNumber > 10 && randomNumber <= 13){
+  } else if (randomNumber > 10){
     return 10
   } else {
     return randomNumber
@@ -24,6 +22,11 @@ function getRandomCard(){
 
 
 function startGame(){
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum += firstCard + secondCard;
   renderGame();
 }
 
@@ -48,10 +51,12 @@ function renderGame(){
 }
 
 function newCard(){
+  if (isAlive && !(hasBlackJack)){
   let card = getRandomCard();
   cards.push(card);
   console.log(cards);
   sum += card;
   renderGame();
+}
 
 }
