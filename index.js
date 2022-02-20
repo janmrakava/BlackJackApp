3d604eecf29fb20a400a0f8ac9f87b6123dd05f6
janmrakava <janmrakava@gmail.com>
2022-02-20 15:26:15 +0100

    Array s kartami

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 11;
-let secondCard = 11;
+let firstCard = 10;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -9,9 +10,14 @@ let sumEl = document.getElementById("sum-el") //už umíme dávno
 //let sumEl = document.querySelector("#sum-el") //Více profesionáln možnost, proto je potřeba být více specifický
 let cardsEl = document.getElementById("cards-el")
 
+
 function startGame(){
+  renderGame();
+}
+
+function renderGame(){
   sumEl.textContent += sum;
-  cardsEl.textContent += firstCard + " " + secondCard;
+  cardsEl.textContent += cards[0] + " " + cards[1];
   if (sum <= 20) {
     message = "Chceš ještě další kartu?";
   } else if (sum === 21) {
@@ -25,9 +31,8 @@ function startGame(){
 }
 
 function newCard(){
-  console.log("Nová karta vytažena");
-}
+  let card = 7;
+  sum += card;
+  renderGame();
 
-console.log(hasBlackJack);
-console.log(isAlive);
-console.log(message);
+}
